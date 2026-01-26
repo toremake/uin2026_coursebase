d3a299b9b51e78f56654585846b18453d80ed10d
ackarlse <anncharlottkarlsen@ymail.com>
2026-01-26 09:54:20 +0100

    Lagt til kommentarer

--- ToDos/src/App.jsx
+++ ToDos/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return (
     <main>
       <h1>Todo app</h1>
+        {/* 
+        Her bruker vi Todos-komponentet.
+        Dette komponentet har ansvar for å hente inn og vise
+        alle enkeltstående todo-elementer, slik at App-komponentet
+        holder seg enkel og oversiktlig.
+      */}
       <Todos />
     </main>
   )

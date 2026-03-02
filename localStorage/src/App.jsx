884297511a042055f3a6d38df11a187340695562
ackarlse <anncharlottkarlsen@ymail.com>
2026-03-02 13:01:53 +0100

    Localstorage

--- localStorage/src/App.jsx
+++ localStorage/src/App.jsx
@@ -7,7 +7,6 @@ import Home from './pages/Home'
 import Movie from './pages/Movie'
 
 function App() {
- 
 
   return (
     <Routes>
--- localStorage/src/pages/Home.jsx
+++ localStorage/src/pages/Home.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home(){
     const [search, setSearch] = useState()
+    const storedHistory = localStorage.getItem("search")
+    
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+
+    console.log("Denne kommer fra storage", storedHistory)
 
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
-    //IKKE GJØR SÅNN!!!!!
-    const apiKey = '9a6e45ff'
+    //GJØR SÅNN!!!!!
+    const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    useEffect(()=>{
+        localStorage.setItem("search", JSON.stringify(history))
+    },[history])
 
     const getMovies = async()=>{
         try
@@ -25,16 +34,27 @@ export default function Home(){
         setSearch(e.target.value)
     }
 
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        e.target.reset()
+
+        setHistory((prev) => [...prev, search])
+
+        
+
+    }
+    console.log(history)
+
     return (
     <main>
         <h1>Forside</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Søk etter film
                 <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
             </label>
-        </form>
-        <button onClick={getMovies}>Søk</button>
+            <button onClick={getMovies}>Søk</button>
+        </form>  
     </main>
         
     )

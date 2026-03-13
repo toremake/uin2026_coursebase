8d468e9e703416ed10ef4eed56d22aa801c42c58
ackarlse <anncharlottkarlsen@ymail.com>
2026-03-13 13:50:58 +0100

    lagt til skisser

--- eksamensoving/pokedex-vanskeligere/src/pages/Home.jsx
+++ eksamensoving/pokedex-vanskeligere/src/pages/Home.jsx
@@ -6,24 +6,25 @@ export default function Home(){
     const [pokemons, setPokemons] = useState()
     const [types, setTypes] = useState()
 
-    const getData = (url, set)=>{
+    const getData = async (url, set)=>{
         const response = await fetch(url)
         const data = await response.json()
         set(data.results)
       
     }
+   
     useEffect(()=>{
         getData('https://pokeapi.co/api/v2/type/', setTypes)
         getData('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9', setPokemons)
     },[])
 
       
-    
     return (
-        <>
+   
         <main>
             <PokemonList pokemons={pokemons} />
             <TypeList types={types} />
         </main>
+  
     )
 }

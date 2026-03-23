f2439b42a75940eb8bb25952385904e5190c17f8
Tore Marius Akerbæk <tore.m.akerbak@hiof.no>
2026-03-23 14:12:23 +0100

    Oppsett av Sanity Sandbox
    Satt opp Sanity Sandbox. Laget innholdstypen "people".

---
+++ SanitySandbox/mysandbox/schemaTypes/people.js
@@ -0,0 +1,11 @@
+const people = {
+    name: "people",
+    title: "Personer",
+    type: "document",
+    fields: [
+        {name: "fullname", title: "Fullt navn", type: "string"},
+        {name: "image", title: "Bilde", type: "image"}
+    ]
+}
+
+export default people
